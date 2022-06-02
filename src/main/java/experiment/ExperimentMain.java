a30297de734da01cefb026cb30657eec426da8e1
catamtz3 <catamtz3@cs.washington.edu>
2022-06-01 23:05:28 -0700

    experiment

--- src/main/java/experiment/ExperimentMain.java
+++ src/main/java/experiment/ExperimentMain.java
@@ -9,18 +9,18 @@ public class ExperimentMain {
     private static CensusGroup[] data;
     final static int NUM_TRIALS = 10;
     final static int NUM_WARMUPS = 4;
-    final static int NUM_QUERIES = 10000;
+    final static int NUM_QUERIES = 1000;
     public static void main(String[] args) {
         data = PopulationQuery.parse("CenPop2010.txt");
         ComplexSequential complexGrid = new ComplexSequential(data, 500, 100);
         SimpleSequential sequentialGrid = new SimpleSequential(data, 500, 100);
         double sequentialTotalTime = 0;
         for (int i = 0; i < NUM_TRIALS; i++) {
-            long startTime = System.currentTimeMillis();
+            long startTime = System.nanoTime();
             for (int j = 0; j < NUM_QUERIES; j++) {
                 sequentialGrid.getPopulation(51, 1, 500, 100);
             }
-            long endTime = System.currentTimeMillis();
+            long endTime = System.nanoTime();
             if (NUM_WARMUPS <= i) {
                 sequentialTotalTime += (endTime-startTime);
             }
@@ -30,12 +30,12 @@ public class ExperimentMain {
 
         double complexTotalTime = 0;
         for (int i = 0; i < NUM_TRIALS; i++) {
-            long start = System.currentTimeMillis();
+            long start = System.nanoTime();
             for (int j = 0; j < NUM_QUERIES; j++) {
                 complexGrid.getPopulation(51, 1, 500, 100);
 
             }
-            long end = System.currentTimeMillis();
+            long end = System.nanoTime();
             if (NUM_WARMUPS <= i) {
                 complexTotalTime += (end-start);
             }
